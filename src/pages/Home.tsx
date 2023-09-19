1bb7e85e1e821c21522b14edafbc93b6c7c47ef6
Toxic254 <cyrus_g@outlook.com>
2023-09-19 11:25:48 +0300

    Created our HomePage

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="text-center p-4 m-0">
+      <h1>Home</h1>
+      <article>Welcome to the home page!</article>
+    </div>
+  );
+}
+
+export default Home;
